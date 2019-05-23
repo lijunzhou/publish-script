0dd948c76dd6c333be734a71fb650555c6fdf0ae
lijun.zhou <zhouj.172@gmail.com>
2019-05-23 16:09:10 +0800

    增加脚本

--- init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /root
-git clone https://github.com/nuls-io/nuls-v2.git
-mv nuls-v2 nuls_2.0
-cd ./nuls_2.0
-git checkout alpha3-test
-./package -mN
-cd ./NULS-Wallet-linux64-alpha2
-cp ./nuls.ncf ./my.ncf
